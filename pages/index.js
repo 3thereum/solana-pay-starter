ef0455f58eb058284e692fb19d27565e682198f0
3thereum <asdf007@live.com>
2022-11-24 03:25:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React from 'react';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import dynamic from "next/dynamic";
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+// Dynamic import `WalletMultiButton` to prevent hydration error
+const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+  );
   
-  
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/YTETMtpsueseikztWR/giphy-downsized-large.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
           <p className="header"> 🫡 Buildspace Emoji Store 😈</p>
@@ -18,7 +37,9 @@ const App = () => {
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/YTETMtpsueseikztWR/giphy-downsized-large.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+
         </main>
 
         <div className="footer-container">
